1696e7c69067e78e1b4992e164d42d7aae9a5cca
TatarinovaEM758 <112684632+TatarinovaEM758@users.noreply.github.com>
2024-11-29 13:09:02 +0300

    Update analyze_aeroflot.py

--- analyze_aeroflot.py
+++ analyze_aeroflot.py
@@ -1,41 +1,39 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, max, min, year, date_format
-from datetime import datetime, timedelta
+from pyspark.sql.functions import col, max, current_date, datediff, expr
 
 # Создание SparkSession
 spark = SparkSession.builder 
-    .appName("AFLT Data Analysis") 
+    .appName("Aeroflot Stock Analysis") 
     .getOrCreate()
 
 # Загрузка данных
-df = spark.read.option("header", "true").csv("AFLT_data.csv")
+file_path = "AFLT.csv"  # Убедитесь, что указали правильный путь к файлу
+data = spark.read.csv(file_path, header=True, inferSchema=True)
 
-# Преобразование столбца "Date" в тип данных Date
-df = df.withColumn("Date", col("Date").cast("date"))
-
-# Установка текущей даты и даты два года назад
-now = datetime.now()
-two_years_ago = now - timedelta(days=730)
+# Выводим схему данных для понимания структуры
+data.printSchema()
+data.show(5)  # Показываем первые 5 строк данных
 
 # Фильтрация данных за последние 2 года
-filtered_data = df.filter(col("Date") >= two_years_ago)
+today_date = current_date()
+two_years_ago = expr('add_months(current_date(), -24)')
+filtered_data = data.filter(col("Дата") >= two_years_ago)
+
+# Приведение столбца к типу Float для вычислений
+filtered_data = filtered_data.withColumn("Цена", col("Цена").cast("float"))
 
 # Расчет максимальной цены закрытия
-max_close = filtered_data.agg(max("Close")).first()[0]
-print(f"Максимальная цена закрытия за последние 2 года: {max_close}")
+max_price = filtered_data.agg(max("Цена")).collect()[0][0]
+print(f"Максимальная цена закрытия за последние 2 года: {max_price}")
 
-# Тренд анализа по месяцам
-# Добавление года и месяца в новую колонку
-trend_analysis = filtered_data.withColumn("YearMonth", date_format(col("Date"), "yyyy-MM"))
+# Подсчет тренда (например, кол-во дней роста и падения)
+daily_trend = filtered_data.withColumn("Изм.", col("Цена").cast("float") - col("Откр.").cast("float"))
 
-# Получение максимальной и минимальной цены закрытия по месяцам
-trend_result = trend_analysis.groupBy("YearMonth").agg(
-    max("Close").alias("Max_Close"),
-    min("Close").alias("Min_Close")
-)
+increasing_days = daily_trend.filter(col("Изм.") > 0).count()
+decreasing_days = daily_trend.filter(col("Изм.") < 0).count()
 
-# Показ результатов
-trend_result.show()
+print(f"Количество дней роста: {increasing_days}")
+print(f"Количество дней падения: {decreasing_days}")
 
-# Завершение работы
+# Остановка Spark
 spark.stop()
